21b7ae9a18676a17a7454b3e6e027757e94452a3
Shubham Singh <coolsks94@gmail.com>
2016-12-29 16:07:26 +0530

    Aex-ify

---
+++ aosp_otus.mk
@@ -0,0 +1,14 @@
+$(call inherit-product, device/motorola/otus/full_otus.mk)
+
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Build Type
+EXTENDED_BUILD_TYPE=OFFICIAL
+
+# Use Magisk
+DEFAULT_ROOT_METHOD := magisk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := aosp_otus
+PRODUCT_RELEASE_NAME := MOTO E 3G
